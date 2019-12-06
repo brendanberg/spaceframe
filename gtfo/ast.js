778e8e0ae3fa27631ac7670b6b2c8da56dcc7779
Brendan Berg <brendan@berg.industries>
2019-12-05 21:55:17 -0500

    add implementations for AST objects

--- gtfo/ast.js
+++ gtfo/ast.js
@@ -29,34 +29,149 @@ Assign.prototype = Object.assign(Object.create(Node.prototype), {
 });
 
 
-function MessageSend(receiver, selector, args, opts) { }
+function MessageSend(receiver, selector, args, opts) {
+	Node.call(this, opts);
+	this.receiver = receiver;
+	this.selector = selector;
+	this.args = args;
+}
+
+MessageSend.prototype = Object.assign(Object.create(Node.prototype), {
+	gmapt: function(fn) {
+		return new this.constructor(
+			fn(this.receiver), fn(this.selector), this.args.map(fn), this.opts
+		);
+	},
+	constructor: MessageSend
+});
 
 
-function SymbolLookup(receiver, symbol, opts) { }
+function SymbolLookup(receiver, symbol, opts) {
+	Node.call(this, opts);
+	this.receiver = receiver;
+	this.symbol = symbol;
+}
+
+SymbolLookup.prototype = Object.assign(Object.create(Node.prototype), {
+	gmapt: function(fn) {
+		return new this.constructor(fn(this.receiver), fn(this.symbol), this.opts);
+	},
+	constructor: SymbolLookup
+});
 
 
-function Subscript(receiver, subscript, opts) { }
+function Subscript(receiver, subscript, opts) {
+	Node.call(this, opts);
+	this.receiver = receiver;
+	this.subscript = subscript;
+}
+
+Subscript.prototype = Object.assign(Object.create(Node.prototype), {
+	gmapt: function(fn) {
+		return new this.constructor(fn(this.receiver), fn(this.subscript), this.opts);
+	},
+	constructor: Subscript
+});
 
 
-function PrefixExpression(oper, expr, opts) { }
+function PrefixExpression(oper, expr, opts) {
+	Node.call(this, opts);
+	this.oper = oper;
+	this.expr = expr;
+}
+
+PrefixExpression.prototype = Object.assign(Object.create(Node.prototype), {
+	gmapt: function(fn) {
+		return new this.constructor(fn(this.oper), fn(this.expr), this.opts);
+	},
+	constructor: PrefixExpression
+});
 
 
-function InfixExpression(oper, leftExpr, rightExpr, opts) { }
+function InfixExpression(oper, lexpr, rexpr, opts) {
+	Node.call(this, opts);
+	this.oper = oper;
+	this.lexpr = lexpr;
+	this.rexpr = rexpr;
+}
+
+InfixExpression.prototype = Object.assign(Object.create(Node.prototype), {
+	gmapt: function(fn) {
+		return new this.constructor(
+			fn(this.oper), fn(this.lexpr), fn(this.rexpr), this.opts
+		);
+	},
+	constructor: InfixExpression
+});
 
 
-function Method(ident, args, block, opts) { }
+function Method(ident, args, block, opts) {
+	Node.call(this, opts);
+	//this.ident = ident;
+	this.args = args;
+	this.block = block;
+}
+
+Method.prototype = Object.assign(Object.create(Node.prototype), {
+	gmapt: function(fn) {
+		return new this.constructor(this.args.map(fn), fn(this.block), this.opts);
+	},
+	constructor: Method
+});
+
+function List(items, opts) {
+	Node.call(this, opts);
+	this.items = items;
+}
+
+List.prototype = Object.assign(Object.create(Node.prototype), {
+	gmapt: function(fn) {
+		return new this.constructor(this.items.map(fn), this.opts);
+	},
+	constructor: List
+});
 
 
-function List(exprs, opts) { }
+function Dictionary(items, opts) {
+	Node.call(this, opts);
+	this.items = items;
+}
+
+Dictionary.prototype = Object.assign(Object.create(Node.prototype), {
+	gmapt: function(fn) {
+		return new this.constructor(this.items.map(fn), this.opts);
+	},
+	constructor: Dictionary
+});
 
 
-function Dictionary(pairs, opts) { }
+function Bottom(opts) {
+	Node.call(this, opts);
+}
+
+Bottom.prototype = Object.assign(Object.create(Node.prototype), {
+	gmapt: function(fn) { return new this.constructor(this.opts); },
+	constructor: Bottom;
+});
 
 
-function Bottom(opts) { }
+function Error(subject, message, consumed, encountered, opts) {
+	Node.call(this, opts);
+	this.subject = subject;
+	this.message = message;
+	this.consumed = consumed;
+	this.encountered = encountered;
+}
 
-
-function Error(opts) { }
+Error.prototype = Object.assign(Object.create(Node.prototype), {
+	gmapt: function(fn) {
+		return new this.constructor(
+			this.subject, this.message, fn(this.consumed),
+			fn(this.encountered), this.opts
+		);
+	},
+	constructor: Error
+});
 
 
 module.exports = {
